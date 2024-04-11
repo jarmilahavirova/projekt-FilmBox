af0d68cd64dca6aa28326d8524da27224924c1a4
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-04-11 18:21:02 +0200

    Hamburger menu setting

---
+++ spolecne.js
@@ -0,0 +1,16 @@
+// definice proměnných
+const menuButton = document.getElementById("menu-tlacitko");
+const menuPolozky = document.getElementById("menu-polozky");
+const menuTlacitko = document.querySelector(".fas");
+
+// hamburger menu
+menuButton.addEventListener("click", function () {
+  document.getElementById("menu-polozky").classList.toggle("show");
+  if (menuPolozky.classList.contains("show")) {
+    menuTlacitko.classList.add("fa-xmark");
+    menuTlacitko.classList.remove("fa-bars");
+  } else {
+    menuTlacitko.classList.add("fa-bars");
+    menuTlacitko.classList.remove("fa-xmark");
+  }
+});
