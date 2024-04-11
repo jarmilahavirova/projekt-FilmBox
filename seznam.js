7b9b17a8d474f0cb63cd6414a2bd8801b805bbc1
JanaDolejsi <jana.dolejsi.gr@gmail.com>
2024-04-11 19:01:29 +0200

    4. Seznam filmů

--- seznam.js
+++ seznam.js
@@ -104,3 +104,31 @@ const filmy = [
 		premiera: '2022-12-24',
 	},
 ]
+
+const seznamFilmuElm = document.querySelector('#seznam-filmu')
+const detailFilmu = filmy.map((film) => {
+	return `<div class="col">
+	<div class="card">
+	  <img
+		src="${film.plakat.url}"
+		width="780"
+		height="520"
+		class="card-img-top"
+		alt="plakát"
+	  />
+	  <div class="card-body">
+		<h5 class="card-title">${film.nazev}</h5>
+		<p class="card-text">${film.ochutnavka}</p>
+		<a href="film.html" class="btn btn-primary">Přehrát</a>
+	  </div>
+	</div>
+  </div>
+  `
+})
+
+const procesDetails = (films) => {
+	// const films = (detailFilmu)
+	seznamFilmuElm.innerHTML = films.join(' ')
+}
+
+procesDetails(detailFilmu)
